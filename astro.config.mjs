2a7a07816277e3a9f8faedea4c0e8c9a00c12670
Juan Franco Vargas <francovargasjuandario@gmail.com>
2025-04-15 16:44:13 -0500

    vercel configuration added

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,18 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import react from '@astrojs/react';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [
-        react()
-    ],
-    vite: {
-        plugins: [tailwindcss()],
-    }
+  integrations: [
+      react()
+  ],
+
+  vite: {
+      plugins: [tailwindcss()],
+  },
+
+  output: 'server',
+  adapter: vercel()
 });
